d00bb4bcd0e20aeb7cf0db198a2a3a0599c82495
jden <jason@denizac.org>
2013-09-13 16:40:19 -0700

    more aggressive char matching patterns

--- index.js
+++ index.js
@@ -1,8 +1,44 @@
 // character codes based on
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/RegExp#endnote_equivalent_s
+// http://msdn.microsoft.com/en-us/library/aa989424(v=vs.80).aspx
+// http://www.cs.tut.fi/~jkorpela/chars/spaces.html
+// and by manually poring over a unicode table
 
-var space = /[ \u00a0\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200a\u3000]/g
-var invisible = /[\u200b\u2028\u2029]/g
+var space = new RegExp('[' + [
+    ' ',
+    '\u3000',
+    '\u2800',
+    '\u205f',
+    '\u202f',
+    '\u2000-\u200a',
+    '\u18aa-\u18af',
+    '\u1878-\u187f',
+    '\u181a-\u181f',
+    '\u180f',
+    '\u0fd9-\u0fff',
+    '\u0fcd',
+    '\u0fbd',
+    '\u0f98',
+    '\u0f8c-\u0f8f',
+    '\u0f6d-\u0f70',
+    '\u0f48',
+    '\u065f',
+    '\u0620',
+    '\u00f7',
+    '\u00a0',
+    '\u0020'
+  ].join('') + ']', 'g')
+var invisible = new RegExp('[' + [
+    '\u206a-\u206f',
+    '\u2060-\u2064',
+    '\u2028-\u202e',
+    '\u200b-\u200f',
+    '\u180b-\u180e',
+    '\u00ad',
+    '\u0080-\u009f',
+    '\u000e-\u001f',
+    '\u0000-\u0008'
+  ].join('') + ']', 'g')
 var tab = /\t/g
 var line = /[\n\v\f\r]/g
 
